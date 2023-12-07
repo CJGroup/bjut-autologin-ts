7f68f563058012a7341812e81c75e29bf492b858
ConstasJ <constasj@qq.com>
2023-12-07 11:15:05 +0800

    refactor: 创建wifi.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { networkInterfaces } from 'node:os';
 
 import { ethernetLogin } from './ethernet';
+import { wifiLogin } from './wifi';
 
 const interfaces = networkInterfaces();
 
 if (interfaces['以太网']) ethernetLogin();
+else if(interfaces['WLAN']) wifiLogin();
 
 export {};
