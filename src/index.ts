379fca061a80ae65b942013659d0663d46b20553
ConstasJ <constasj@qq.com>
2023-12-07 11:06:54 +0800

    refactor: 删除抓包用环境变量

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import { networkInterfaces } from 'node:os';
 
 import { ethernetLogin } from './ethernet';
 
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-
 const interfaces = networkInterfaces();
 
 if (interfaces['以太网']) ethernetLogin();
